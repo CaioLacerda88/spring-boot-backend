135af3f6af1e3fc2687e61ea55ca6a12d13774fc
Caio Lacerda <caiolacerda88@gmail.com>
2018-08-04 14:45:02 -0300

    Estado e Cidade

---
+++ src/main/java/com/caiolacerda/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.caiolacerda.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.caiolacerda.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
